69f50f124fc40c2297db72972d80cd80573dae0a
Diego Martinez <55361667+dmartinezm@users.noreply.github.com>
2020-03-08 14:27:25 -0400

    first commit

---
+++ dist/index.js
@@ -0,0 +1,18 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+// Palindrome
+// Is a string that is the same in reversed order
+// Example: kayak, radar, level, rotor, madam
+// Task: create a function that given a string will determine (true/false)
+// if it's a palindrome. (include spaces)
+
+function palindrome(s) {
+  return s;
+}
+
+console.log(palindrome("hello"));
+exports.default = palindrome;
+// module.exports = palindrome;
