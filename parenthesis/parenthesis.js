bcb21c4fe4bb84f5c503805607788299993f98e8
Diego Martinez <55361667+dmartinezm@users.noreply.github.com>
2020-03-15 22:06:29 -0400

    created blank function and initial test

---
+++ parenthesis/parenthesis.js
@@ -0,0 +1,9 @@
+// Create a function that will return YES/NO
+// if the string of parenthesis ()[]{} is balanced
+// example : {[()]} -> YES {[(])} -> NO {{[[(())]]}} -> YES
+
+function isBalanced(s) {
+  return s;
+}
+
+module.exports = isBalanced;
---
+++ parenthesis/parenthesis.test.js
@@ -0,0 +1,5 @@
+const balanced = require("./parenthesis");
+
+test("isBalanced is a function", () => {
+  expect(typeof balanced).toEqual("function");
+});
